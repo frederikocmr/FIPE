b6c5a4cd798d81303e1d7f3b8f948cac2399f2c2
Frederiko C. M. Ribeiro <frederiko.cmr@gmail.com>
2019-09-14 15:11:47 -0300

    Project base created and configured

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StatusBar, View } from 'react-native';
+import './config/ReactotronConfig';
+
+export default function App() {
+  return (
+    <>
+      <StatusBar backgroundColor="#F6F7F9" barStyle="dark-content" />
+      <View />
+    </>
+  );
+}
