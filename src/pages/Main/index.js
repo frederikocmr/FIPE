4a207ed711dc9bc68312dd721ce9983484ea18c7
Frederiko C. M. Ribeiro <frederiko.cmr@gmail.com>
2019-09-15 03:30:26 -0300

    Added Axios and API request

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,9 @@
-import { ActivityIndicator, Image } from 'react-native';
+import { ActivityIndicator, Text, Image, ToastAndroid } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+
+import api from '../../services/api';
 
 import {
   Container,
@@ -42,30 +44,9 @@ class Main extends Component {
       cars: [
         {
           id: '1',
-          brand: 'Chevrolet',
-          model: 'Sonic',
-          transmission: 'Manual',
-          price: 'R$ 92.799,00',
-        },
-        {
-          id: '2',
-          brand: 'Chevrolet',
-          model: 'Cobalt',
-          transmission: 'Automático',
-          price: 'R$ 92.799,00',
-        },
-        {
-          id: '3',
-          brand: 'Chevrolet',
-          model: 'S10',
-          transmission: 'Manual',
-          price: 'R$ 92.799,00',
-        },
-        {
-          id: '4',
-          brand: 'Chevrolet',
-          model: 'Celta',
-          transmission: 'Manual',
+          brand: 'Marca',
+          model: 'Modelo',
+          transmission: 'Transmissão',
           price: 'R$ 92.799,00',
         },
       ],
@@ -73,9 +54,108 @@ class Main extends Component {
     };
   }
 
+  async componentDidMount() {
+    try {
+      const randomBrand = await this.getRandomBrand();
+
+      if (randomBrand !== null) {
+        const cars = await this.getCars(randomBrand);
+
+        this.setState({ cars });
+      }
+    } catch (error) {
+      this.showError('no servidor', error);
+    }
+  }
+
+  getRandomBrand = async () => {
+    try {
+      const { data } = await api.get('/marcas');
+
+      const brands = data.map(value => ({
+        name: value.nome,
+        id: value.codigo,
+      }));
+
+      const max = brands.length - 1;
+
+      const arrayPosition = parseInt(Math.random() * (max + 1), 10);
+
+      const brand = brands[arrayPosition];
+
+      return brand;
+    } catch (error) {
+      this.showError('as marcas', error);
+
+      return null;
+    }
+  };
+
+  getCars = async brand => {
+    try {
+      const { data } = await api.get(`/marcas/${brand.id}/modelos`);
+      console.tron.log(data);
+
+      const cars = data.modelos.map(value => ({
+        id: String(value.codigo),
+        model: this.getCarName(value.nome.split(' ')),
+        brand: brand.name,
+        transmission: this.getCarTransmission(value.nome.split(' ')),
+        price: 'R$ 92.799,00',
+      }));
+
+      console.tron.log(cars);
+
+      return cars.slice(0, 10);
+    } catch (error) {
+      this.showError('os modelos', error);
+      return [];
+    }
+  };
+
+  getCarName = value => {
+    let name = '';
+
+    for (let i = 0; i < 3; i += 1) {
+      if (Number(value[i]) > 0 || !value[i]) {
+        name += '';
+      } else {
+        name += `${value[i]} `;
+      }
+    }
+    return name;
+  };
+
+  getCarTransmission = value => {
+    if (value.indexOf('Aut.') >= 0) {
+      return 'Automático';
+    }
+    if (value.indexOf('Man.') >= 0) {
+      return 'Manual';
+    }
+    return null;
+  };
+
+  showError = (text, error) => {
+    ToastAndroid.show(
+      `Aconteceu algum erro ao buscar ${text}. Por favor, tente novamente mais tarde!`,
+      ToastAndroid.LONG
+    );
+
+    console.tron.log(error);
+  };
+
   handleSearch = async () => {
-    const { carModel } = this.state;
-    console.tron.log(carModel);
+    const { carModel, cars } = this.state;
+
+    cars.push({
+      id: '1133',
+      brand: 'Marca',
+      model: 'Modelo',
+      transmission: 'Transmissão',
+      price: 'R$ 92.799,00',
+    });
+    this.setState({ cars, carModel });
   };
 
   handleNavigate = car => {
@@ -103,14 +183,17 @@ class Main extends Component {
             {loading ? (
               <ActivityIndicator color="#FFF" />
             ) : (
-                <Image source={SearchImage} />
-              )}
+              <Image source={SearchImage} />
+            )}
           </SubmitButton>
         </Form>
 
         <List
           data={cars}
           keyExtractor={car => car.id}
+          onEndReached={({ distanceFromEnd }) => {
+            console.tron.log(distanceFromEnd);
+          }}
           renderItem={({ item }) => (
             <TouchableOpacity
               onPress={() => {
@@ -122,21 +205,27 @@ class Main extends Component {
                 <Info>
                   <Brand>{item.brand}</Brand>
                   <Model>{item.model}</Model>
-                  <Transmission>
-                    <TransmissionIcon
-                      source={
-                        item.transmission === 'Manual'
-                          ? SettingsImage
-                          : LightingImage
-                      }
-                    />
-                    <TransmissionText>{item.transmission}</TransmissionText>
-                  </Transmission>
+                  {item.transmission ? (
+                    <Transmission>
+                      <TransmissionIcon
+                        source={
+                          item.transmission === 'Manual'
+                            ? SettingsImage
+                            : LightingImage
+                        }
+                      />
+                      <TransmissionText>{item.transmission}</TransmissionText>
+                    </Transmission>
+                  ) : (
+                    <Text> &nbsp; </Text>
+                  )}
                   <PriceStaticText>Preço</PriceStaticText>
                   <Price>
                     <PriceText small>{item.price.substring(0, 2)}</PriceText>
-                    <PriceText>{item.price.substring(3, 9)}</PriceText>
-                    <PriceText small>{item.price.substring(9, 12)}</PriceText>
+                    <PriceText>
+                      {item.price.split(' ')[1].split(',')[0]}
+                    </PriceText>
+                    <PriceText small>{item.price.slice(-3)}</PriceText>
                   </Price>
                 </Info>
               </Car>
